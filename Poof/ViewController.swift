61f7b090333821128360e32aa5e8f01f066c40dc
usagimaru <usagimaruma@gmail.com>
2023-03-01 07:41:02 +0900

    Initial Commit

---
+++ Poof/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Poof
+//
+//  Created by usagimaru on 2023/03/01.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
